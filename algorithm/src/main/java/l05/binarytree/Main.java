b9e80a5324bc49b1a4936132fa4a2246bf8716f9
zhangxiaojun10 <zhangxiaojun10@meituan.com>
2024-04-07 00:34:17 +0800

    add bst

--- algorithm/src/main/java/l05/binarytree/Main.java
+++ algorithm/src/main/java/l05/binarytree/Main.java
@@ -1,7 +1,53 @@
 package l05.binarytree;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
+
     public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        Random random = new Random();
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
+        }
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            nums.add(bst.removeMax());
+        }
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) < nums.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+        System.out.println("removeMax test completed.");
+    }
+
+
+    public static void main2(String[] args) {
+        BST<Integer> bst = new BST<>();
+        Random random = new Random();
+        int n = 1000;
+        for (int i = 0; i < n; i++) {
+            bst.add(random.nextInt(10000));
+        }
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            nums.add(bst.removeMin());
+        }
+        System.out.println(nums);
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums.get(i - 1) > nums.get(i)) {
+                throw new IllegalArgumentException("Error");
+            }
+        }
+
+        System.out.println("removeMin test completed.");
+    }
+
+    public static void main1(String[] args) {
         BST<Integer> bst = new BST<>();
         int[] nums = {5, 3, 6, 8, 4, 2};
         for (int num : nums)
@@ -22,7 +68,6 @@ public class Main {
 
         bst.levelOrder();
         System.out.println();
-
 
 
         bst.inOredr();
